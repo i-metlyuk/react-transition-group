6f8e0912058a18c95986a309970ab91be9dcec92
i-metlyuk <igor.m@core-tech.ru>
2022-01-11 10:07:37 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
 
+const Modal = (props) => {
+
+    return (
+        <div className="modal mt-5 d-block">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title">Typical modal window</h5>
+                    <button onClick={() => props.onClose(false)} type="button" className="btn-close" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                    <p>Modal body content</p>
+                </div>
+                <div className="modal-footer">
+                    <button onClick={() => props.onClose(false)} type="button" className="btn btn-secondary">Close</button>
+                    <button onClick={() => props.onClose(false)} type="button" className="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showModal, setShowModal] = useState(false);
+
+    return (
+        <Container>
+            {showModal ? <Modal onClose={setShowModal}/> : null}
+            <button 
+                type="button" 
+                className="btn btn-warning mt-5"
+                onClick={() => setShowModal(true)}>Open Modal</button>
+        </Container>
+    );
 }
 
 export default App;
